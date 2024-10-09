fdea262dc87e044b0789d1c0e34d491e168ba33b
EscapedShadows <web@escapedshadows.com>
2024-10-09 18:47:07 +0200

    Added Server Script

---
+++ server.py
@@ -0,0 +1,64 @@
+from flask import Flask, request, jsonify
+import time
+import json
+from datetime import datetime
+
+app = Flask(__name__)
+
+def get_saved_messages(date):
+    formatted_date = datetime.fromtimestamp(date)
+    formatted_date = formatted_date.strftime("%Y-%m-%d")
+
+    with open(f'messages/{formatted_date}.json', 'r') as f:
+        data = json.load(f)
+
+    return data
+
+def add_message(author, message):
+    formatted_date = time.strftime("%Y-%m-%d", time.localtime())
+    timestamp = time.time()
+
+    file_path = f'messages/{formatted_date}.json'
+    
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        data = {"messages": []}
+
+    data["messages"].append({
+        "author": author,
+        "message": message,
+        "timestamp": timestamp
+    })
+
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
+
+@app.route('/')
+def credits():
+    json_response = {"name": "EscapedShadows", "url": "actuallysecurechatapp.escapedshadows.com"}
+    return jsonify(json_response)
+
+@app.route('/sendMessage', methods=['POST'])
+def send_message():
+    data = request.json
+    print(data)
+
+    author = data["author"]
+    message = data["message"]
+
+    add_message(author, message)
+
+    return jsonify({"message": "success"}), 201
+
+@app.route('/getMessages', methods=['POST'])
+def get_messages():
+    data = request.json
+    date = data["date"]
+
+    messages = get_saved_messages(date)
+
+    return jsonify(messages)
+
+app.run(host="0.0.0.0", debug=True)
